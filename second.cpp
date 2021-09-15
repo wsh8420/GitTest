1e00ee110b36857a011562dc2b45dbef6d6f8795
wsh8420 <wsh8420>
2021-09-15 23:21:09 +0800

    second add

---
+++ second.cpp
@@ -0,0 +1,5 @@
+//
+// Created by watson on 2021/9/15.
+//
+
+#include "second.h"
---
+++ second.h
@@ -0,0 +1,14 @@
+//
+// Created by watson on 2021/9/15.
+//
+
+#ifndef GITTEST_SECOND_H
+#define GITTEST_SECOND_H
+
+
+class second {
+
+};
+
+
+#endif //GITTEST_SECOND_H
